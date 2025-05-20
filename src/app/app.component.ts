eacd7666d486679dc456d725ced3c6699c5f3020
Cleiton Sampaio <cleiton.nso2019@gmail.com>
2025-05-20 10:37:46 -0300

    criando site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
@@ -10,6 +10,21 @@ import { IntegrationsComponent } from './components/integrations/integrations.co
 import { MethodologyComponent } from './components/methodology/methodology.component';
 import { ServicesComponent } from './components/services/services.component';
 
+// src/typings.d.ts
+interface Window {
+  chatwootSettings?: {
+    position: string;
+    type: string;
+    launcherTitle: string;
+  };
+  chatwootSDK?: {
+    run: (config: {
+      websiteToken: string;
+      baseUrl: string;
+    }) => void;
+  };
+}
+
 @Component({
   selector: 'app-root',
   imports: [HeaderComponent, FooterComponent, HeroComponent, AboutComponent, ServicesComponent, IntegrationsComponent, BenefitsComponent, MethodologyComponent, CtaComponent],
@@ -17,6 +32,37 @@ import { ServicesComponent } from './components/services/services.component';
   styleUrl: './app.component.scss',
   standalone: true
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'digital-fluxo-tech';
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.loadChatwootScript();
+  }
+
+  private loadChatwootScript(): void {
+    // Definindo as configurações do Chatwoot no objeto window
+    (window as any).chatwootSettings = {
+      position: 'right',
+      type: 'standard',
+      launcherTitle: 'Fale conosco no chat'
+    };
+
+    const script = this.renderer.createElement('script');
+    script.src = 'https://chat.fluxodigitaltech.com.br/packs/js/sdk.js';
+    script.defer = true;
+    script.async = true;
+    
+    script.onload = () => {
+      if ((window as any).chatwootSDK) {
+        (window as any).chatwootSDK.run({
+          websiteToken: 'qSbYYAQDt5hj2xVazaWCQ1yU',
+          baseUrl: 'https://chat.fluxodigitaltech.com.br'
+        });
+      }
+    };
+
+    this.renderer.appendChild(document.body, script);
+  }
 }
